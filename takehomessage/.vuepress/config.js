352c33fb3432ac2e4576912cf723e80ca46a8e4e
hubuntu <hq@live.cn>
2019-11-29 19:31:55 -0500

    add takehomessage

---
+++ takehomessage/.vuepress/config.js
@@ -0,0 +1,73 @@
+module.exports = {
+  title: 'Take Home Message',
+  description: 'A tips/notes collection',
+  theme: require.resolve('../../'),
+  themeConfig: {
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#nav
+     */
+    nav: [
+      {
+        text: 'Blog',
+        link: '/',
+      },
+      {
+        text: 'Tags',
+        link: '/tag/',
+      },
+      {
+        text: 'About',
+        link: '/about/',
+      },
+    ],
+    /**
+     * Ref: https://vuepress-theme-blog.ulivz.com/#footer
+     */
+    footer: {
+      contact: [
+        {
+          type: 'github',
+          link: 'https://github.com/hubentu',
+        },
+        {
+          type: 'twitter',
+          link: 'https://twitter.com/hubentu',
+        },
+        {
+          type: 'web',
+          link: 'https://hubentu.github.io/others/Rcwl/',
+        }
+      ],
+      copyright: [
+        {
+          text: 'Privacy Policy',
+          link: 'https://policies.google.com/privacy?hl=en-US',
+        },
+        {
+          text: 'MIT Licensed | Copyright © 2019-present',
+          link: '',
+        },
+      ],
+    },
+
+    modifyBlogPluginOptions (blogPluginOptions) {
+      const sitemap = {
+        hostname: 'https://takehomessage'
+      }
+
+      const comment = {
+        service: 'disqus',
+        shortname: 'vuepress-plugin-blog',
+        // service: 'vssue',
+        // owner: 'You',
+        // repo: 'Your repo',
+        // clientId: 'Your clientId',
+        // clientSecret: 'Your clientSecret',
+      }
+
+      return { ...blogPluginOptions, sitemap, comment }
+    }
+
+    // paginationComponent: 'SimplePagination'
+  },
+}
---
+++ takehomessage/_posts/2019-11-29-what-is-takehomessage-for.md
@@ -0,0 +1,12 @@
+---
+date: 2019-11-29
+tag: 
+  - blog
+  - takehomessage
+author: Qiang
+location: Buffalo
+---
+
+# What is TakeHoMessage for?
+
+**Take Home Message** is a collection of short tip/note for #rstats #DataScience #Bioformatics and more.
